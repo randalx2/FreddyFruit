806f99d013ed9de6ac7ea22caf58e10f26df5994
Praneel Misthry <pmisthry@gmail.com>
2018-07-04 13:18:46 +0200

    Add domain classes and seed initializers

---
+++ FreddyFruit/Models/ProductContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace FreddyFruit.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("FreddyFruit")
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
