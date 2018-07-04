de02224e69131fd607455cfb6b516fa27ba0b2bc
Praneel Misthry <pmisthry@gmail.com>
2018-07-04 13:02:25 +0200

    Add project files.

---
+++ FreddyFruit/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace FreddyFruit.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ FreddyFruit/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FreddyFruit.Startup))]
+namespace FreddyFruit
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
