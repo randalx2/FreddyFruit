3fe3721414860aec93c70624e8c63c026c730ba5
Praneel Misthry <pmisthry@gmail.com>
2018-07-07 20:46:57 +0200

    add CartItem and update product context

--- FreddyFruit/Models/ProductContext.cs
+++ FreddyFruit/Models/ProductContext.cs
@@ -15,5 +15,7 @@ namespace FreddyFruit.Models
         public DbSet<Category> Categories { get; set; }
 
         public DbSet<Product> Products { get; set; }
+
+        public DbSet<CartItem> ShoppingCartItems { get; set; }
     }
 }
