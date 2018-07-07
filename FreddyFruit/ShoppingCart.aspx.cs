b15e260a8211fd9d6be21d219fb2c6dfbb2fb063
Praneel Misthry <pmisthry@gmail.com>
2018-07-08 01:21:19 +0200

    updating checkout

--- FreddyFruit/ShoppingCart.aspx.cs
+++ FreddyFruit/ShoppingCart.aspx.cs
@@ -33,6 +33,7 @@ namespace FreddyFruit
                     lblTotal.Text = "";
                     ShoppingCartTitle.InnerText = "Shopping Cart is Empty";
                     UpdateBtn.Visible = false;
+                    CheckoutImageBtn.Visible = false;
                 }
             }
         }
@@ -88,6 +89,15 @@ namespace FreddyFruit
         protected void UpdateBtn_Click(object sender, EventArgs e)
         {
             UpdateCartItems();
+        }
+
+        protected void CheckoutBtn_Click(object sender, ImageClickEventArgs e)
+        {
+            using (ShoppingCartActions usersShoppingCart = new ShoppingCartActions())
+            {
+                Session["payment_amt"] = usersShoppingCart.GetTotal();
+            }
+            Response.Redirect("Checkout/CheckoutStart.aspx");
         }
     }
 }
