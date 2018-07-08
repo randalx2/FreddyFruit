74c95cb9d37e1ba088710cbec3803dd13ec35de0
Praneel Misthry <pmisthry@gmail.com>
2018-07-08 17:13:29 +0200

    reseeded the db

--- FreddyFruit/Models/ProductDatabaseInitializer.cs
+++ FreddyFruit/Models/ProductDatabaseInitializer.cs
@@ -8,7 +8,9 @@ namespace FreddyFruit.Models
 {
     //Used to seed the database with test data
 
-    public class ProductDatabaseInitializer : DropCreateDatabaseIfModelChanges<ProductContext>
+    //TODO Inherit from DropCreateDatabaseIfModelChanges<ProductContext> after resetting the db
+
+    public class ProductDatabaseInitializer : DropCreateDatabaseAlways<ProductContext>
     {
         protected override void Seed(ProductContext context)
         {
@@ -22,28 +24,13 @@ namespace FreddyFruit.Models
                 new Category
                 {
                     CategoryID = 1,
-                    CategoryName = "Cars"
+                    CategoryName = "Fruits"
                 },
                 new Category
                 {
                     CategoryID = 2,
-                    CategoryName = "Planes"
-                },
-                new Category
-                {
-                    CategoryID = 3,
-                    CategoryName = "Trucks"
-                },
-                new Category
-                {
-                    CategoryID = 4,
-                    CategoryName = "Boats"
-                },
-                new Category
-                {
-                    CategoryID = 5,
-                    CategoryName = "Rockets"
-                },
+                    CategoryName = "Vegetables"
+                }
             };
 
             return categories;
@@ -55,151 +42,38 @@ namespace FreddyFruit.Models
                 new Product
                 {
                     ProductID = 1,
-                    ProductName = "Convertible Car",
-                    Description = "This convertible car is fast! The engine is powered by a neutrino based battery (not included)." +
-                                  "Power it up and let it go!",
-                    ImagePath="carconvert.png",
-                    UnitPrice = 22.50,
+                    ProductName = "Apples",
+                    Description = "Our apples are just what you need to keep the dentist away." 
+                                  + " Sweet, succulent with a shiny red colour that will be enough to tempt Adam himself.",
+                    ImagePath="apples.jpg",
+                    UnitPrice = 6.30,
                     CategoryID = 1
                },
+
                 new Product
                 {
                     ProductID = 2,
-                    ProductName = "Old-time Car",
-                    Description = "There's nothing old about this toy car, except it's looks. Compatible with other old toy cars.",
-                    ImagePath="carearly.png",
-                    UnitPrice = 15.95,
-                     CategoryID = 1
+                    ProductName = "Bananas",
+                    Description = "No need to swing from trees like Tarzan. " 
+                                  + "Just place an order with us and we will satisfy your cravings for this soft fruit within one business day."
+                                  + " Just don't let your girlfriend be seen eating one of our bananas in public as they are yuuuuge.",
+                    ImagePath="Bananas.jpg",
+                    UnitPrice = 4.30,
+                    CategoryID = 1
                },
+
                 new Product
                 {
                     ProductID = 3,
-                    ProductName = "Fast Car",
-                    Description = "Yes this car is fast, but it also floats in water.",
-                    ImagePath="carfast.png",
-                    UnitPrice = 32.99,
-                    CategoryID = 1
-                },
-                new Product
-                {
-                    ProductID = 4,
-                    ProductName = "Super Fast Car",
-                    Description = "Use this super fast car to entertain guests. Lights and doors work!",
-                    ImagePath="carfaster.png",
-                    UnitPrice = 8.95,
-                    CategoryID = 1
-                },
-                new Product
-                {
-                    ProductID = 5,
-                    ProductName = "Old Style Racer",
-                    Description = "This old style racer can fly (with user assistance). Gravity controls flight duration." +
-                                  "No batteries required.",
-                    ImagePath="carracer.png",
-                    UnitPrice = 34.95,
-                    CategoryID = 1
-                },
-                new Product
-                {
-                    ProductID = 6,
-                    ProductName = "Ace Plane",
-                    Description = "Authentic airplane toy. Features realistic color and details.",
-                    ImagePath="planeace.png",
-                    UnitPrice = 95.00,
+                    ProductName = "Coconuts",
+                    Description = "Is it a fruit or vegetable? Our web developer wasn't sure. " 
+                                  + "So we just decided to charge you an arm and a leg for these. It ain't easy getting guys to climb up tall trees for these without insurance." 
+                                  + " Besides..we know you really need this because your mom needs something to throw on the ground til it breaks!",
+                    ImagePath="Coconuts.jpg",
+                    UnitPrice = 82.99,
                     CategoryID = 2
                 },
-                new Product
-                {
-                    ProductID = 7,
-                    ProductName = "Glider",
-                    Description = "This fun glider is made from real balsa wood. Some assembly required.",
-                    ImagePath="planeglider.png",
-                    UnitPrice = 4.95,
-                    CategoryID = 2
-                },
-                new Product
-                {
-                    ProductID = 8,
-                    ProductName = "Paper Plane",
-                    Description = "This paper plane is like no other paper plane. Some folding required.",
-                    ImagePath="planepaper.png",
-                    UnitPrice = 2.95,
-                    CategoryID = 2
-                },
-                new Product
-                {
-                    ProductID = 9,
-                    ProductName = "Propeller Plane",
-                    Description = "Rubber band powered plane features two wheels.",
-                    ImagePath="planeprop.png",
-                    UnitPrice = 32.95,
-                    CategoryID = 2
-                },
-                new Product
-                {
-                    ProductID = 10,
-                    ProductName = "Early Truck",
-                    Description = "This toy truck has a real gas powered engine. Requires regular tune ups.",
-                    ImagePath="truckearly.png",
-                    UnitPrice = 15.00,
-                    CategoryID = 3
-                },
-                new Product
-                {
-                    ProductID = 11,
-                    ProductName = "Fire Truck",
-                    Description = "You will have endless fun with this one quarter sized fire truck.",
-                    ImagePath="truckfire.png",
-                    UnitPrice = 26.00,
-                    CategoryID = 3
-                },
-                new Product
-                {
-                    ProductID = 12,
-                    ProductName = "Big Truck",
-                    Description = "This fun toy truck can be used to tow other trucks that are not as big.",
-                    ImagePath="truckbig.png",
-                    UnitPrice = 29.00,
-                    CategoryID = 3
-                },
-                new Product
-                {
-                    ProductID = 13,
-                    ProductName = "Big Ship",
-                    Description = "Is it a boat or a ship. Let this floating vehicle decide by using its " +
-                                  "artifically intelligent computer brain!",
-                    ImagePath="boatbig.png",
-                    UnitPrice = 95.00,
-                    CategoryID = 4
-                },
-                new Product
-                {
-                    ProductID = 14,
-                    ProductName = "Paper Boat",
-                    Description = "Floating fun for all! This toy boat can be assembled in seconds. Floats for minutes!" +
-                                  "Some folding required.",
-                    ImagePath="boatpaper.png",
-                    UnitPrice = 4.95,
-                    CategoryID = 4
-                },
-                new Product
-                {
-                    ProductID = 15,
-                    ProductName = "Sail Boat",
-                    Description = "Put this fun toy sail boat in the water and let it go!",
-                    ImagePath="boatsail.png",
-                    UnitPrice = 42.95,
-                    CategoryID = 4
-                },
-                new Product
-                {
-                    ProductID = 16,
-                    ProductName = "Rocket",
-                    Description = "This fun rocket will travel up to a height of 200 feet.",
-                    ImagePath="rocket.png",
-                    UnitPrice = 122.95,
-                    CategoryID = 5
-                }
+               
             };
 
             return products;
